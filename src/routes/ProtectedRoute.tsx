c01021d8e79512b39cc49d11eaabe8e03ea7a2aa
ViktoriaElm <v.gorkina.job@gmail.com>
2026-04-26 00:39:24 +0300

    add file for admin

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { Route, redirect } from 'react-router-dom';
+
+// interface ProtectedRouteProps {
+//   isAuthenticated: boolean;
+//   redirectPath: string;
+//   children: React.ReactNode;
+// }
+
+// const ProtectedRoute = ({ isAuthenticated, redirectPath, children }: ProtectedRouteProps) => {
+//   if (!isAuthenticated) {
+//     return <redirect to={redirectPath} />;
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+
+
+
+
+
+
+// реализовать для админки
