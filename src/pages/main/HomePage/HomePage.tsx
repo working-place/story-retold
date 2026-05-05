846bbdc94913f8290adfe610001ea4b515b37e83
ViktoriaElm <v.gorkina.job@gmail.com>
2026-05-05 15:31:45 +0300

    test

--- src/pages/main/HomePage/HomePage.tsx
+++ src/pages/main/HomePage/HomePage.tsx
@@ -45,6 +45,7 @@ export default function HomePage() {
                     </h2>
                     <p
                         // style={{ fontFamily: 'Metamorphous-Regular', fontWeight: 400 }}
+                        // style={{ fontFamily: 'Metamorphous-Regular', fontWeight: 400 }}
                         className={styles.history_description__firstLine}>
                             All about this font-style that <br /> cannot afford latin. number of 600 <br />
                         Мы — команда МАОУ «СОШ № 60» города <br />
