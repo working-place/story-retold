68cc27ff7416999653ad8dcd36ffa5eae43b1402
ViktoriaElm <v.gorkina.job@gmail.com>
2026-06-03 01:47:08 +0300

    fix: error TS2559: Type '{ children: Element; }' has no properties in common with type 'IntrinsicAttributes'

--- src/pages/admin/AdminHeroActions/AdminHeroActions.tsx
+++ src/pages/admin/AdminHeroActions/AdminHeroActions.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from "react";
-import AdminLayout from "../../../components/layout/AdminLayout/AdminLayout";
+// import AdminLayout from "../../../components/layout/AdminLayout/AdminLayout";
 import styles from "./AdminHeroActions.module.scss"
 import { NavLink } from "react-router-dom";
 
@@ -10,11 +10,11 @@ import { NavLink } from "react-router-dom";
 export default function AdminHeroActions( ): JSX.Element {
 
     return (
-        <AdminLayout>
+        // <AdminLayout>
             <div className={styles.adminHeroActions}>
                 <h1>AdminHeroActions</h1>
                 <NavLink to="/">Главная</NavLink>
             </div>
-        </AdminLayout>
+        // </AdminLayout>
     )
 }
