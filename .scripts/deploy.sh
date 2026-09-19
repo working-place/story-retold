5507c73b0ef5124487ed6a29139ab009ca736e0b
Andrew Filkin <filkin.andrew.s@gmail.com>
2026-09-19 10:17:47 +0300

    Fix script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm use default
-
-set -e
-
 echo "Production Deployment started "
 
 git reset --hard
